b441ee55800e964928fdd8c433935bec40e70b95
JohnMarvelousDiaz <133172952+JohnMarvelousDiaz@users.noreply.github.com>
2024-12-14 22:04:35 +0800

    Update Resume v2

--- src/components/main_components/home.vue
+++ src/components/main_components/home.vue
@@ -15,7 +15,7 @@
          <div class="w-[570px] flex flex-col gap-y-2 mb-12">
             <span class="text-sm text-tprimary">{{ description }}</span>
          </div>
-         <a href="https://drive.google.com/file/d/1Lxay7MVFyaJUUaBTF9bb6xSfxK55JX85/view?usp=drive_link"
+         <a href="https://drive.google.com/file/d/1X-GYDLOoV4Q0L5xD7FyVBhpZffxPTLEC/view?usp=drive_link"
             target="_blank">
             <button type="button" class="w-max h-auto flex items-center px-5 py-3 rounded-full text-tprimary button-bg">
                View Resume <Icon class="ml-2"
